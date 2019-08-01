e78eb969401a68b15aa404239f3712ba16400588
jasoncfinley <jasoncfinley@gmail.com>
2019-07-31 19:16:19 -0500

    Add project files.

---
+++ Sudoku/Class1.vb
@@ -0,0 +1,17 @@
+﻿Public Class Sudoku
+    Private _RealGrid(,) As String
+
+    Public Sub SetGrid(grid(,) As String)
+        _RealGrid = grid
+
+        Dim test As String
+
+        test = grid(8, 8)
+        test = 18
+        MsgBox("8,8 : " & grid(8, 8))
+    End Sub
+End Class
+
+Public Class GroupOfNine
+
+End Class
---
+++ Sudoku/Module1.vb
@@ -0,0 +1,19 @@
+﻿Module GridFunctions
+    Public Function ConvertToGrid(ByVal value As String()) As Array
+        If value Is Nothing Then
+            Throw New ArgumentNullException(NameOf(value))
+        End If
+
+        Dim grid(8, 8) As String
+        Dim current = 0
+
+        For i = 0 To 8
+            For j = 0 To 8
+                grid(i, j) = value(current)
+                current = current + 1
+            Next
+        Next
+
+        Return grid
+    End Function
+End Module
